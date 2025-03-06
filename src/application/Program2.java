f6e00c87d24ff1f06e8f005759b2f7992b94f592
Xamacardoso <xcardoso1997@gmail.com>
2025-03-06 03:53:31 -0300

    Feat (Model/Dao): Added DepartmentDaoJDBC insertion

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -14,10 +14,14 @@ public class Program2 {
         Department retrievedDepartment = departmentDao.findById(3).get();
         System.out.println(retrievedDepartment);
 
-        System.out.println("\n==== TEST 2 - RETRIEVING A DEPARTMENT ====");
+        System.out.println("\n==== TEST 2 - RETRIEVING ALL DEPARTMENTS ====");
         List<Department> allDepartments = departmentDao.findAll();
         allDepartments.forEach(System.out::println);
 
+        System.out.println("\n==== TEST 3 - INSERTING A DEPARTMENT ====");
+        Department newDepartment = new Department(null, "D1");
+        departmentDao.insert(newDepartment);
+        System.out.println("Insertion done! New department: " + newDepartment);
 
     }
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -19,7 +19,29 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void insert(Department department) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
+        try {
+            ps = conn.prepareStatement("""
+            INSERT INTO department (Name)
+            VALUES (?)""", Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, department.getName());
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected > 0) {
+                rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    department.setId(rs.getInt(1));
+                }
+
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(ps);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
